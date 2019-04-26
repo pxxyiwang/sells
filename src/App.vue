aee467ea57675786b9fd458e1b48d62eaf13502b
pxxyiwang <18290411696@163.com>
2019-04-26 18:09:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- <div class="harde">I am header</div> -->
+    <Header></Header>
+    <div class="tap">I am tap</div>
+    <div class="content">I an content</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Header from './components/header/header.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+</style>
