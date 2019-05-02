590a6793466823e671ee5a75b790e821b2ff8c71
pxxyiwang <18290411696@163.com>
2019-05-02 08:57:31 +0800

    App.vue的提交

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,75 @@
 <template>
   <div id="app">
-    <!-- <div class="harde">I am header</div> -->
-    <Header></Header>
-    <div class="tap">I am tap</div>
-    <div class="content">I an content</div>
+    <div class="header">
+      <Header v-bind:seller="seller"></Header>
+    </div>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="seller">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import Header from './components/header/header.vue'
+import Header from "./components/header/header";
+
+const ERR_OK = 0;
 
 export default {
+  name: "App",
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get("/api/seller").then(response => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = response.data;
+        console.log(this.seller);
+      }
+    });
+  },
   components: {
     Header
   }
-}
+};
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/styuls">
+@import './common/stylus/mixin.styl';
+
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  /* position: relative;
+  border: none; */
+  border-1px(rgba(7, 17, 27, 0.1));
+
+  .tab-item {
+    flex: 1;
+    text-align: center;
+
+    & > a {
+      display: block;
+      font-size: 14px;
+      color: rgb(77, 86, 93);
+
+      &.active {
+        color: rgb(240, 20, 20);
+      }
+    }
+  }
+}
 </style>
